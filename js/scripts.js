24e4bf4691362f02c347209d1d0f69d8fa125ad5
Blake Scala <blakescala@gmail.com>
2016-08-26 10:37:11 -0700

    split cost function into sizecost and toppingCost, write prototype functions for each

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,40 @@
-function Pizza(toppings, size, delivery, price) {
+function Pizza(toppings, size, delivery) {
   this.toppings = toppings;
   this.size = size;
   this.delivery = false;
-  this.price = price
+  this.price = 0;
 }
+
 Pizza.prototype.sizeCost = function(){
-  var totalCost = 0;
-  if (this.size = "Small"){
-    totalCost += 5;
-  } else if (this.size = "Medium"){
-    totalCost += 8;
-  } else if (this.size = "Large"){
-    totalCost += 11;
-  } else if (this.size = "Extra Large"){
-    totalCost += 15;
+  if (this.size === "Small"){
+    this.price += 5;
+  } else if (this.size === "Medium"){
+    this.price += 8;
+  } else if (this.size === "Large"){
+    this.price += 11;
+  } else if (this.size === "Extra Large"){
+    this.price += 15;
   }
-  for (var i = 0; i < this.toppings; i++) {
-    totalCost += 3;
-  }
-  if (this.delivery === true) {
-  totalCost += 5;}
+}
+
+Pizza.prototype.toppingCost = function() {
+  var multiplier = (((this.toppings).length) * 2);
+  console.log("m " + multiplier);
+  console.log(this.price);
+  this.price += multiplier;
+  console.log(this.price);
 }
 
 $(document).ready(function() {
-  $(".pizzabuilder").submit(function(event) {
+  $("form").submit(function(event) {
     event.preventDefault();
+    var sizeInput = $("input:radio[name=size-check]:checked").val();
+    var toppingsArray = [];
+    $("input:checkbox[name=topping-check]:checked").each(function(){
+    toppingsArray.push($(this).val());
+    var newPizza = new Pizza(toppingsArray, sizeInput, false);
+    newPizza.sizeCost();
+    newPizza.toppingCost();
+    });
   });
 });
