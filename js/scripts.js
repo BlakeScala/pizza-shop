321669e20a78547761e06e7666a7f6cb52507156
Blake Scala <blakescala@gmail.com>
2016-08-26 11:55:07 -0700

    add output div that displays the user's order

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//Back End
 function Pizza(toppings, size, delivery) {
   this.toppings = toppings;
   this.size = size;
@@ -5,7 +6,7 @@ function Pizza(toppings, size, delivery) {
   this.price = 0;
 }
 
-Pizza.prototype.sizeCost = function(){
+Pizza.prototype.pizzaCost = function(){
   if (this.size === "Small"){
     this.price += 5;
   } else if (this.size === "Medium"){
@@ -15,26 +16,33 @@ Pizza.prototype.sizeCost = function(){
   } else if (this.size === "Extra Large"){
     this.price += 15;
   }
+  var addToppings = (((this.toppings).length) * 2);
+  this.price += addToppings;
 }
 
-Pizza.prototype.toppingCost = function() {
-  var multiplier = (((this.toppings).length) * 2);
-  console.log("m " + multiplier);
-  console.log(this.price);
-  this.price += multiplier;
-  console.log(this.price);
-}
-
+//Front End
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     var sizeInput = $("input:radio[name=size-check]:checked").val();
     var toppingsArray = [];
-    $("input:checkbox[name=topping-check]:checked").each(function(){
+    $("input:checkbox[name=topping-check]:checked").each(function() {
     toppingsArray.push($(this).val());
+    });
     var newPizza = new Pizza(toppingsArray, sizeInput, false);
-    newPizza.sizeCost();
-    newPizza.toppingCost();
+    newPizza.pizzaCost();
+    toppingOutput = newPizza.toppings;
+    $("#output").append("<li>" + newPizza.size + " pizza with " + toppingOutput + ": $" + newPizza.price + "</li>");
+  });
+
+  $("#addpizza").click(function(event){
+    $("input:radio[name='size-check']").each(function(i) {
+       this.checked = false;
+    });
+    $("input:checkbox[name='topping-check']").each(function(i) {
+       this.checked = false;
     });
   });
+
+
 });
