d5586ba300c4e05caac2dd6261b24eaf863ff1c1
Blake Scala <blakescala@gmail.com>
2016-08-26 08:33:45 -0700

    write prototype function for calculating cost

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,23 @@ function Pizza(toppings, size, delivery, price) {
   this.delivery = false;
   this.price = price
 }
-function Topping(name, price){
-  this.name = name;
-  this.price = price;
+Pizza.prototype.sizeCost = function(){
+  var totalCost = 0;
+  if (this.size = "Small"){
+    totalCost += 5;
+  } else if (this.size = "Medium"){
+    totalCost += 8;
+  } else if (this.size = "Large"){
+    totalCost += 11;
+  } else if (this.size = "Extra Large"){
+    totalCost += 15;
+  }
+  for (var i = 0; i < this.toppings; i++) {
+    totalCost += 3;
+  }
+  if (this.delivery === true) {
+  totalCost += 5;}
 }
-function Size(Price,Slices){
-  this.price = price;
-  this.slices = slices;
-}
-
 
 $(document).ready(function() {
   $(".pizzabuilder").submit(function(event) {
