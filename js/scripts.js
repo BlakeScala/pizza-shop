86bc5435c81e9bbf20b77c4fae3f26a039bd9467
Blake Scala <blakescala@gmail.com>
2016-08-26 14:59:18 -0700

    add pictures to jumbotron, write scripts for delivery option, put in address form

--- js/scripts.js
+++ js/scripts.js
@@ -2,47 +2,71 @@
 function Pizza(toppings, size, delivery) {
   this.toppings = toppings;
   this.size = size;
-  this.delivery = false;
+  this.delivery = delivery;
   this.price = 0;
 }
-
 Pizza.prototype.pizzaCost = function(){
-  if (this.size === "Small"){
+  console.log(this.price);
+  if (this.size === "small"){
     this.price += 5;
-  } else if (this.size === "Medium"){
+  } else if (this.size === "medium"){
     this.price += 8;
-  } else if (this.size === "Large"){
+  } else if (this.size === "large"){
     this.price += 11;
-  } else if (this.size === "Extra Large"){
+  } else if (this.size === "extra large"){
     this.price += 15;
   }
+  console.log(this.price);
   var addToppings = (((this.toppings).length) * 2);
   this.price += addToppings;
+  console.log(this.price);
+  if (this.delivery === "delivery"){
+    this.price += 5;
+  } else if (this.delivery === "pickup"){
+    this.price += 0;
+  }
 }
 
 //Front End
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
+    var nameInput = $("#inputname").val();
     var sizeInput = $("input:radio[name=size-check]:checked").val();
     var toppingsArray = [];
     $("input:checkbox[name=topping-check]:checked").each(function() {
     toppingsArray.push($(this).val());
     });
-    var newPizza = new Pizza(toppingsArray, sizeInput, false);
+    var deliveryInput= $("input:radio[name=delivery-check]:checked").val();
+    var newPizza = new Pizza(toppingsArray, sizeInput, deliveryInput);
     newPizza.pizzaCost();
+
     toppingOutput = newPizza.toppings;
-    $("#output").append("<li>" + newPizza.size + " pizza with " + toppingOutput + ": $" + newPizza.price + "</li>");
+    $("#output").text("Hello, " + nameInput + "! You ordered a " + newPizza.size + " pizza for " + newPizza.delivery + " with " + toppingOutput + ": $" + newPizza.price);
+    $("#yourorder").show();
+
+    if (deliveryInput === "delivery"){
+      $("#address").show();
+      $("#pickup").hide();
+    }
   });
 
-  $("#addpizza").click(function(event){
+  $("#reset").click(function(){
     $("input:radio[name='size-check']").each(function(i) {
        this.checked = false;
     });
     $("input:checkbox[name='topping-check']").each(function(i) {
        this.checked = false;
     });
+    $("#yourorder").hide();
+    $("#address").hide();
   });
 
+  $("#addressconfirm").submit(function(){
+    $("#thanks").show();
+  });
 
+  $("#pickupconfirm").click(function(){
+    $("#thanks").show();
+  })
 });
